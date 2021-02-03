d01fce299530627107eab613b2a7341862768476
ThibaultLanthiez <65160656+ThibaultLanthiez@users.noreply.github.com>
2021-02-03 21:10:32 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ import random # Piocher au hasard des images
 import os # Acces aux fonctions systèmes 
 from PIL import Image # Manipulation d'images
 import pandas as pd
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 
