ed4920c6fd6248bc007e7a088176d594ef80f0f9
ThibaultLanthiez <65160656+ThibaultLanthiez@users.noreply.github.com>
2021-02-04 09:35:11 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -10,40 +10,43 @@ import numpy as np
 
 from PIL import Image
 import numpy as np
-#from skimage import transform
+from skimage import transform
 
 st.write("""
 # My web application : whales tail
 """)
 
 uploaded_file = st.file_uploader("Upload Files",type=['jpg','png','jpeg'])
-image = Image.open(uploaded_file)
 
-data = pd.read_csv('train.csv')
-    
-st.write(f"Nom du fichier : {uploaded_file.name}")
-st.write(f"Taille : {image.size}")
-id_bal = data[data['Image'] == uploaded_file.name]['Id']
-st.write(id_bal)
-st.image(image,width=500,height=500)
+if uploaded_file != None:
 
-model = load_model('model.hdf5')
+   image = Image.open(uploaded_file)
 
-#def load(filename):
-#   np_image = Image.open(filename)
-#   np_image = np.array(np_image).astype('float32')/255
-#   np_image = transform.resize(np_image, (224, 224, 3))
-#   np_image = np.expand_dims(np_image, axis=0)
-#   return np_image
+   data = pd.read_csv('train.csv')
+      
+   st.write(f"Nom du fichier : {uploaded_file.name}")
+   st.write(f"Taille : {image.size}")
+   id_bal = data[data['Image'] == uploaded_file.name]['Id']
+   st.write(id_bal)
+   st.image(image,width=700,height=700)
+   '''
+   model = load_model('model.hdf5')
 
-#image = load('train/'+image_name)
-#st.write(model.predict(image))
+   def load(np_image):
+      np_image = np.array(np_image).astype('float32')/255
+      np_image = transform.resize(np_image, (224, 224, 3))
+      np_image = np.expand_dims(np_image, axis=0)
+      return np_image
 
-#pred = model.predict_classes(image)
+   image = load(image)
+   st.write(model.predict(image))
 
-#class_indices = np.load('dict_encodage.npy',
-#                        allow_pickle=True).item()
+   pred = model.predict_classes(image)
 
-#st.write(f'Prédiction : {list(class_indices.keys())[pred.tolist()[0]]}')
-#st.write(f'Num réalité : {class_indices[id_bal.tolist()[0]]}')
+   class_indices = np.load('dict_encodage.npy',
+                           allow_pickle=True).item()
 
+   st.write(f'Prédiction : {list(class_indices.keys())[pred.tolist()[0]]}')
+   st.write(f'Num réalité : {class_indices[id_bal.tolist()[0]]}')
+
+   '''
